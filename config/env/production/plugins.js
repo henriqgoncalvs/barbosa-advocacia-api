70dcf6d7b9843de787c9b375189aa4a478a65acb
Henrique <henriquesg09@hotmail.com>
2021-07-01 19:07:30 -0300

    Main: add aws keys

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
     providerOptions: {
       accessKeyId: env('AWS_ACCESS_KEY_ID'),
       secretAccessKey: env('AWS_ACCESS_SECRET'),
-      region: env('AWS_BUCKET'),
+      region: env('AWS_REGION'),
       params: {
-        Bucket: env('AWS_REGION'),
+        Bucket: env('AWS_BUCKET'),
       },
     },
   },
