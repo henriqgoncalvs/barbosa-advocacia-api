dc480bb5a7ce463d9be670371562797f920dad19
Henrique <henriquesg09@hotmail.com>
2021-07-02 16:53:55 -0300

    Main: add footer and profile_picture

--- config/database.js
+++ config/database.js
@@ -5,12 +5,12 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL'),
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', '5432'),
+        database: env('DATABASE_NAME', 'barbosastrapi'),
+        username: env('DATABASE_USERNAME', 'barbosastrapi'),
+        password: env('DATABASE_PASSWORD', 'barbosastrapi'),
+        ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
     },
